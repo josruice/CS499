e3bcf7b095047f0c0740235feb3459b8c88b3150
José Vicente Ruiz <josruice@upv.es>
2014-04-09 14:33:10 -0500

    Added some stats to the code to check the performance of the classification.

--- Code/automarkup.m
+++ Code/automarkup.m
@@ -35,6 +35,8 @@ img_format = 'png';
 
 markup_file = '../Markups/Machine-Markup-(1.0).txt';
 
+num_properties_per_image = 6;
+
 % Classifiers.
 feature_method = 'SIFT'; % PHOW, SIFT or DSIFT.
 
@@ -58,7 +60,7 @@ run('/Users/Josevi/Libraries/vlfeat-0.9.18/toolbox/vl_setup');
 % Variables to improve code legibility.
 num_materials = length(materials);
 num_file_names = length(file_names);
-
+num_images = num_materials * num_file_names;
 
 %%% TRAINING %%%
 
@@ -108,8 +110,13 @@ for i = 1:num_properties,
     % Store everything in the cell data structure.
     svms{i} = {scale, feature, W, B};
 
+    % Elements with score 0 (on the line of the linear classifier) are
+    % the same as negative (don't have the property).
+    estimated_labels = sign(scores); % Returns -1, 0 or 1, depending on sign.
+    estimated_labels( find(estimated_labels == 0) ) = -1;
+
     % Testing accuracy in the training set.
-    accuracy = sum(labels == sign(scores')) / length(labels);
+    accuracy = sum(labels == sign(estimated_labels')) / length(labels);
     mean_accuracy = mean_accuracy + accuracy;
     min_accuracy = min(min_accuracy, accuracy);
     max_accuracy = max(max_accuracy, accuracy);
@@ -140,7 +147,7 @@ features_2d( find(features_2d) ) = 1; % Binary histograms.
 
 % 3D matrix to store the binary properties vector of the images. The rows
 % represent the materials, the columns the file names and the depth the 
-% properties.
+% properties with value 0 (not present) or 1 (present).
 est_properties = zeros(num_materials, num_file_names, num_properties);
 
 % Use one vs. all multiclass classification.
@@ -154,8 +161,16 @@ for i = 1:num_properties,
     B = svms{i}{4};
     scores = (W' * features_2d) + B;
 
+    % Elements with score 0 (on the line of the linear classifier) are
+    % the same as negative (don't have the property).
+    estimated_labels = sign(scores); % Returns -1, 0 or 1, depending on sign.
+    estimated_labels( find(estimated_labels == 0) ) = -1;
+
+    % Store the estimated properties of each image using -1 and 1.
+    est_properties(:,:,i) = reshape(estimated_labels, [num_file_names, num_materials])'; 
+
     % Testing accuracy in the test set.
-    accuracy = sum(labels == sign(scores')) / length(labels);
+    accuracy = sum(labels == sign(estimated_labels')) / length(labels);
     mean_accuracy = mean_accuracy + accuracy;
     min_accuracy = min(min_accuracy, accuracy);
     max_accuracy = max(max_accuracy, accuracy);
@@ -168,4 +183,97 @@ fprintf(1, ' - Mean accuracy: %.2f\n', mean_accuracy * 100);
 fprintf(1, ' - Min accuracy: %.2f\n', min_accuracy * 100);
 fprintf(1, ' - Max accuracy: %.2f\n\n', max_accuracy * 100);
 
-%keyboard;
+% Convert the estimated properties vectors to binary (-1 -> 0 and 1 -> 1).
+est_properties( find(est_properties == -1) ) = 0;
+
+keyboard;
+
+% Print some statistics about the current estimated properties.
+%  - Materials, images and properties (real and estimated).
+fprintf(1, 'Number of materials: %d\n', num_materials);
+fprintf(1, 'Number of images per material: %d\n', num_file_names);
+fprintf(1, 'Number of properties per image: %d\n', num_properties_per_image);
+fprintf(1, 'Number of different properties: %d\n', num_properties);
+fprintf(1, '\n');
+
+fprintf(1, 'Total number of images: %d\n', num_images);
+total_num_properties = num_images * num_properties_per_image;
+fprintf(1, 'Total number of properties: %d\n', total_num_properties);
+total_num_est_properties = sum(est_properties(:));
+fprintf(1, 'Total number of estimated properties: %d\n', total_num_est_properties);
+total_size_est_properties = prod(size(est_properties));
+fprintf(1, 'Total size of vectors of properties: %d\n', total_size_est_properties);
+fprintf(1, '\n');
+
+fprintf(1, 'Proportion between number of estimated properties and number of properties: %.3f\n', total_num_est_properties/total_num_properties);
+fprintf(1, 'Proportion between number of estimated properties and total size of vectors of properties: %.3f\n', total_num_est_properties/total_size_est_properties);
+fprintf(1, '\n');
+
+%  - Check which images of different materials share the same vector of
+%    properties.
+fprintf(1, 'List of images that share the same vector of properties:\n');
+same_vector_counter = 0;
+same_prop_matrix = zeros(num_materials, num_file_names);
+for i = 1:num_materials, for j = 1:num_file_names,
+    for i2 = i+1:num_materials, for j2 = 1:num_file_names,
+        if isequal(est_properties(i,j,:), est_properties(i2,j2,:))
+            same_vector_counter = same_vector_counter+1;
+            same_prop_matrix(i,j) = same_prop_matrix(i,j) + 1;
+            same_prop_matrix(i2,j2) = same_prop_matrix(i2,j2) + 1;
+            fprintf(1, '   %3d - %s %s is equal to %s %s\n', same_vector_counter, materials{i}, file_names{j}, materials{i2}, file_names{j2});
+        end
+    end; end
+end; end
+
+%  - Show some information about how the vector of properties are shared.
+num_images_share = length(find(same_prop_matrix));
+fprintf(1, '\n');
+fprintf(1, 'Number of images that share their vector of properties with images from other classes: %d of a total of %d images (%.2f%%).\n', num_images_share, num_images, num_images_share*100/num_images);
+fprintf(1, 'Number of images that don''t share their vector of properties with images from other classes: %d of a total of %d images (%.2f%%).\n', num_images-num_images_share, num_images, 100-(num_images_share*100/num_images));
+
+
+%  - Get the materials that share the properties vector the most.
+max_shared_material_times = max(sum(same_prop_matrix'));
+max_shared_material_names = materials(find(sum(same_prop_matrix') == max_shared_material_times));
+fprintf(1, 'The material(s) that share(s) the properties vector of their images the most: \n')
+disp(sort(max_shared_material_names)')
+fprintf(1, 'with a total of %d times.\n', max_shared_material_times);
+fprintf(1, '\n');
+
+%  - Get the materials that share the properties vector the less.
+min_shared_material_times = min(sum(same_prop_matrix'));
+min_shared_material_names = materials(find(sum(same_prop_matrix') == min_shared_material_times));
+fprintf(1, 'The material(s) that share(s) the properties vector of their images the less: \n')
+disp(sort(min_shared_material_names)')
+fprintf(1, 'with a total of %d times.\n', min_shared_material_times);
+fprintf(1, '\n');
+
+%  - Get the average times that a material share their properties vector.
+avg_shared_material_times = mean(sum(same_prop_matrix'));
+fprintf(1, 'The average times that a material share their properties vector is %.2f.\n', avg_shared_material_times)
+fprintf(1, '\n');
+
+%  - Get the images that share the vector of properties the most.
+max_shared_image_times = max(same_prop_matrix(:));
+[I,J] = ind2sub([num_materials, num_file_names], find(same_prop_matrix == max(same_prop_matrix(:))));
+max_shared_image_names = strcat(materials(I), '-', file_names(J));
+fprintf(1, 'The image(s) that share(s) the properties vector the most: \n')
+disp(sort(max_shared_image_names)');
+fprintf(1, 'with a total of %d times.\n', max_shared_image_times);
+fprintf(1, '\n');
+
+%  - Get the images that share the vector of properties the less.
+min_shared_image_times = min(same_prop_matrix(:));
+[I,J] = ind2sub([num_materials, num_file_names], find(same_prop_matrix == min(same_prop_matrix(:))));
+min_shared_image_names = strcat(materials(I), '-', file_names(J));
+fprintf(1, 'The image(s) that share(s) the properties vector the less: \n')
+disp(sort(min_shared_image_names)');
+fprintf(1, 'with a total of %d times.\n', min_shared_image_times);
+fprintf(1, '\n');
+
+%  - Get the average times that an image shares its properties vector.
+avg_shared_image_times = mean(same_prop_matrix(:));
+fprintf(1, 'The average times that an image shares its properties vector is %.2f.\n', avg_shared_image_times)
+fprintf(1, '\n');
+
+keyboard;
